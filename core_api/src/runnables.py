8c0ca5ba07dd0435c9f6b5748446ade8accf531f
Will Langdale <will.langdale@trade.gov.uk>
2024-06-12 13:00:53 +0100

    Initial map reduce steps

--- core_api/src/runnables.py
+++ core_api/src/runnables.py
@@ -3,7 +3,7 @@ from operator import itemgetter
 from langchain.schema import StrOutputParser
 from langchain_community.chat_models import ChatLiteLLM
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import Runnable, RunnableLambda
+from langchain_core.runnables import Runnable, RunnableLambda, RunnablePassthrough
 
 from core_api.src.format import format_chunks
 
@@ -15,6 +15,8 @@ def make_stuff_document_runnable(
     """Takes a system prompt and LLM returns a stuff document runnable.
 
     Runnable takes input of a dict keyed to question, messages and documents.
+
+    Runnable returns a dict keyed to response and sources.
     """
     chat_history = [
         ("system", system_prompt),
@@ -22,7 +24,7 @@ def make_stuff_document_runnable(
         ("user", "Question: {question}. \n\n Documents: \n\n {documents} \n\n Answer: "),
     ]
 
-    return (
+    stuff_doc_chain = (
         {
             "question": itemgetter("question"),
             "messages": itemgetter("messages"),
@@ -31,4 +33,12 @@ def make_stuff_document_runnable(
         | ChatPromptTemplate.from_messages(chat_history)
         | llm
         | StrOutputParser()
+    )
+
+    return (
+        RunnablePassthrough()
+        | {
+            "response": stuff_doc_chain,
+            "sources": itemgetter("documents")
+        }
     )
